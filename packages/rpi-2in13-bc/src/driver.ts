58ed01f5a0ab5a3f7e78ff2a08428001b9c4d4de
Robert Byrden <62345622+rob-byrden@users.noreply.github.com>
2026-06-20 17:18:58 +0100

    Delete packages/rpi-2in13-bc directory

--- packages/rpi-2in13-bc/src/driver.ts
+++
@@ -1,8 +0,0 @@
-export interface Driver {
-    dev_init(): void;
-    init(): void;
-    display(blackBuffer: Buffer, colorBuffer: Buffer): void;
-    clear(): void;
-    sleep(): void;
-    dev_exit(): void;
-}
